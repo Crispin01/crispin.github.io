7a88c0f05d961b0338a7b3c35b2c0772281f1d9a
AndresCV <102707188+Crispin01@users.noreply.github.com>
2026-02-04 20:28:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
+// Archivo: script.js
 const panels = document.querySelectorAll('.panel');
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
-        panels.forEach(p => p.classList.remove('active'));
+        removeActiveClasses();
         panel.classList.add('active');
     });
 });
-const panels = document.querySelectorAll('.panel');
-        panels.forEach(panel => {
-            panel.addEventListener('click', () => {
-                removeActiveClasses();
-                panel.classList.add('active');
-            });
-        });
-        function removeActiveClasses() {
-            panels.forEach(panel => {
-                panel.classList.remove('active');
-            });
-        }
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    });
+}
